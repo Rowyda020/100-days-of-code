8cb1f5a6f00a3b097a21d67923278a4344c68634
Rowyda Ramadan <76496105+Rowyda020@users.noreply.github.com>
2023-03-31 16:47:49 +0200

    Update Band-Name-Generator.py

--- Band-Name-Generator.py
+++ Band-Name-Generator.py
@@ -1,5 +1,5 @@
 #1. Create a greeting for your program.
-print("Hi! This is band-name-generator\n")
+print("Hi! This is band-name-generator")
 #2. Ask the user for the city that they grew up in.
 city_name =input("What is the name of the city you grew up in? \n")
 #3. Ask the user for the name of a pet.
@@ -7,3 +7,4 @@ pet_name = input("What is your pet's name? \n")
 #4. Combine the name of their city and pet and show them their band name.
 band_name = city_name + pet_name
 print("Your band name is "+ band_name)
+
